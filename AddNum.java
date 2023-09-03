e44b6a970e70eabf3319879ecfda3915f9914483
Manish9440 <manishk94402gmail.com>
2023-09-03 17:48:03 +0530

    First commit

---
+++ AddNum.java
@@ -0,0 +1,13 @@
+package in.manish.main;
+
+public class AddNum {
+
+	public static void main(String[] args) {
+		int a=2, b=5, sum;
+		sum=a+b;
+		System.out.println("sum of two no:"+sum);
+		
+
+	}
+
+}
---
+++ MainApp.java
@@ -0,0 +1,10 @@
+package in.manish.main;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
