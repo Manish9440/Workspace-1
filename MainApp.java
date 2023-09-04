12600b7ef42fa9db09644129ce5c104e982977f1
Manish9440 <manishk94402gmail.com>
2023-09-04 20:08:05 +0530

    changes in MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -3,7 +3,7 @@ package in.manish.main;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello mkkk");
+		System.out.println("Hello change in this");
 
 	}
 
